bddc26feb706fbd97780af7ca50b6814993bba3b
Andrew van der Westhuizen <avanderw@gmail.com>
2022-11-30 19:05:59 +0200

    Update to read from new private api-key

--- v2/bookmarks.js
+++ v2/bookmarks.js
@@ -1,6 +1,6 @@
 function updateMeta(e) {
     const id = e.id;
-    const meta = JSON.parse(localStorage.getItem(id)) || {"clicks":0,"last":"never"};
+    const meta = JSON.parse(localStorage.getItem(id)) || { "clicks": 0, "last": "never" };
     meta.clicks += 1;
     meta.last = new Date().toISOString();
     localStorage.setItem(id, JSON.stringify(meta))
@@ -9,7 +9,7 @@ function updateMeta(e) {
 
 function score(item) {
     const id = md5(item.title);
-    const meta = JSON.parse(localStorage.getItem(id)) || {"clicks":0,"last":"never"};
+    const meta = JSON.parse(localStorage.getItem(id)) || { "clicks": 0, "last": "never" };
     const dayDifference = Math.floor((new Date() - new Date(meta.last)) / (1000 * 60 * 60 * 24)) || 0;
     return Math.floor(meta.clicks * 100 / (dayDifference + 1));
 }
@@ -19,7 +19,7 @@ function domain(url) {
 }
 
 const BOOKMARK_TEMPLATE =
-  "<li><a id='${id}' href='${url}' onclick='updateMeta(this)' target='_blank'>${title} <span>(${domain})</span></a><div>Score: ${score} | Last used: ${last}</div></li>";
+    "<li><a id='${id}' href='${url}' onclick='updateMeta(this)' target='_blank'>${title} <span>(${domain})</span></a><div>Score: ${score} | Last used: ${last}</div></li>";
 
 let data = BOOKMARK_DATA;
 data.sort((a, b) => {
@@ -29,7 +29,7 @@ data.sort((a, b) => {
 let html = ``;
 for (const item of data) {
     let id = md5(item.title);
-    const meta = JSON.parse(localStorage.getItem(id)) || {"clicks":0,"last":"never"};
+    const meta = JSON.parse(localStorage.getItem(id)) || { "clicks": 0, "last": "never" };
 
     html += `${BOOKMARK_TEMPLATE
         .replace(/\$\{id\}/g, id)
@@ -37,14 +37,15 @@ for (const item of data) {
         .replace(/\$\{domain\}/g, domain(item.url))
         .replace(/\$\{title\}/g, item.title)
         .replace(/\$\{score\}/g, score(item))
-        .replace(/\$\{last\}/g, meta.last.replace(/T.*$/,""))}`;
+        .replace(/\$\{last\}/g, meta.last.replace(/T.*$/, ""))}`;
 }
 document.getElementById("bookmarks").innerHTML = html;
 
 if (localStorage.getItem("last-clear") === null) {
     localStorage.setItem("last-clear", new Date().toISOString());
 }
-document.getElementById("footer").innerHTML = `Last cleared: ${localStorage.getItem("last-clear").replace(/T.*$/,"")}`;
+document.getElementById("footer").innerHTML = `Last cleared: ${localStorage.getItem("last-clear").replace(/T.*$/, "")}`;
+
 
 const encodedParams = new URLSearchParams();
 encodedParams.append("my-url", "https://xkcd.com/info.0.json");
@@ -53,7 +54,7 @@ const options = {
 	method: 'POST',
 	headers: {
 		'content-type': 'application/x-www-form-urlencoded',
-		'X-RapidAPI-Key': '',
+		'X-RapidAPI-Key': API_KEY,
 		'X-RapidAPI-Host': 'cors-proxy3.p.rapidapi.com'
 	},
 	body: encodedParams

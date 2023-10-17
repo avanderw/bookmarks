dc779ffb178e72b0170b296c2cdbdfa90d870568
Andrew van der Westhuizen <avanderw@gmail.com>
2023-10-17 14:56:42 +0200

    Add conversion from old format to new format

--- src/lib/bookmarks.ts
+++ src/lib/bookmarks.ts
@@ -17,13 +17,43 @@ export interface BookmarkStore {
     bookmarks: Bookmark[];
 }
 
+interface OldBookmark {
+    href: string;
+    title: string | null;
+    description: string | null;
+    tags: string[];
+    notes: string | null;
+    added: Date;
+    clicked: number | null;
+    last: Date | null;
+}
+
 const version = "2023-10-16";
-let data: BookmarkStore = { version: version, bookmarks: [] };
+let data: BookmarkStore | Array<OldBookmark> = { version: version, bookmarks: [] };
 if (browser) {
+    console.log("Loading bookmark store");
     const localStore = localStorage.getItem("bookmarks");
 
     if (localStore) {
         data = JSON.parse(localStore);
+
+        if (data instanceof Array) {
+            console.log("Converting unversioned bookmark store to new format");
+            const tmp = data;
+            data = { version: version, bookmarks: [] };
+            for (const bookmark of tmp) {
+                data.bookmarks.push({
+                    url: bookmark.href,
+                    title: bookmark.title,
+                    description: "",
+                    tags: [],
+                    notes: "",
+                    added: bookmark.added,
+                    clicked: bookmark.clicked,
+                    last: bookmark.last
+                });
+            }
+        }
     }
 }
 
@@ -33,5 +63,5 @@ appData.subscribe((value) => {
     if (browser)
         localStorage.setItem("bookmarks", JSON.stringify(value));
 }); // local storage
-appData.subscribe((value) => { console.log("Updating bookmark store\n", value); }); // for debugging
+appData.subscribe((value) => { console.log("Saving bookmark store\n", value); }); // for debugging
 // update filehandle
